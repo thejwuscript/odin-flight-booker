4bc07a41a382718d963498e0fccdf66509500b52
James Wu <ichangjwu@outlook.com>
2022-04-19 10:22:14 +0900

    Add four dropdown menus

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    @departure_options = Flight.all.map { |flight| [flight.departure_airport.name, flight.departure_airport.id] }.uniq
+    @arrival_options = Flight.all.map { |flight| [flight.arrival_airport.name, flight.arrival_airport.id] }.uniq
+    @dates = Flight.all.map { |flight| flight.time }
+    @years = @dates.map { |date| date.strftime("%Y") }.uniq
+    @months = @dates.map { |date| date.strftime("%B") }.uniq
+    @days = @dates.map { |date| date.strftime("%d") }.uniq
   end
 end
