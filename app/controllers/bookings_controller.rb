ea13cc29b5c6d64c9083207a40dfe373360b6a7a
James Wu <ichangjwu@outlook.com>
2022-04-21 02:33:31 +0900

    Add basic functions of creating/showing a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,24 @@
 class BookingsController < ApplicationController
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
-    @booking.flight = @flight
     # build_booking?
     num = params[:travellers].to_i
     num.times { @booking.travellers.build }
   end
 
   def create
+    @booking = Booking.create(booking_params)
+    redirect_to @booking
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, travellers_attributes: [:name, :email])
   end
 end
